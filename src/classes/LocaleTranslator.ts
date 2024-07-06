f0ed17da9c21b169d83ede1d1494669c02b71b28
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-06 16:34:45 +0100

    improve error handler & fix some bugs

--- src/classes/LocaleTranslator.ts
+++ src/classes/LocaleTranslator.ts
@@ -14,7 +14,13 @@ export class LocaleTranslator {
 	}
 
 	public translate(key: string, variables?: Record<string, string>) {
-		const result = this.moduleObject?.[key];
+		if (!this.moduleObject) {
+			console.error(`TRANSLATOR_ERROR: LOCALE "${this.locale}" MODULE "${this.options.module}" NOT LOADED.`);
+
+			return `TRANSLATOR_ERROR: LOCALE "${this.locale}" MODULE "${this.options.module}" NOT LOADED. KEY: "${key}"`;
+		}
+
+		const result = this.moduleObject[key];
 
 		if (!result)
 			console.error(`TRANSLATION_ERROR: LOCALE "${this.locale}" KEY "${key}" MODULE "${this.options.module}"`);
